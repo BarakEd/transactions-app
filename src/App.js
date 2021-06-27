cfc67c619a5e08b2a362b14079ee957cad2d7228
BarakEd <33124134+BarakEd@users.noreply.github.com>
2021-06-27 08:29:36 +0300

    Revert "Fetch data"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import { useFetchTransactions } from './hooks';
-import TransactionsPage from './components/TransactionsPage';
 
 function App() {
-  const { transactions } = useFetchTransactions();
-  
   return (
     <div className="App">
-      <TransactionsPage transactions={ transactions }/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
