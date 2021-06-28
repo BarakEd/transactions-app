ae11376cf72bf24d7feb156ca30bfc4e73988df8
barak <barak.eden@devalore.com>
2021-06-29 00:13:15 +0300

    implement compress

--- src/components/TransactionsPage/TransactionsPage.js
+++ src/components/TransactionsPage/TransactionsPage.js
@@ -3,54 +3,46 @@ import TransactionsTable from '../TransactionsTable/TransactionsTable';
 import AddTransactionDialog from '../AddTransactionDialog';
 import Button from '../Button';
 import { useFetchTransactions } from '../../hooks';
-import { TRANSACTIONS_URL } from '../../constants/urls';
+import { addAction, compressAction } from '../../utils';
 import styles from './TransactionsPage.style';
 
 const TransactionsPage = () => {
-  const { transactions } = useFetchTransactions();
+  const transactions = useFetchTransactions();
   const [isAddDialogOpen, setIsAddDialogOpen] = useState(false);
   const [transactionsData, setTransactionsData] = useState(transactions);
-  console.log(transactions);
 
  useEffect(() => {
   setTransactionsData(transactions);
   }, [transactions]);
   
-  const addTransactionHandler = async (tradingParty, counterParty, amount) => {
-    console.log(tradingParty, counterParty, amount);
-
-    const res = await fetch(TRANSACTIONS_URL, {
-      method: 'POST',
-      body: JSON.stringify({ tradingParty, counterParty, amount }),
-      mode: 'cors',
-    })
-    .then(res => res.json())
-    .catch(e => console.error(e));
-
-    console.log(res);
-    if(res?.transactions?.length){
-      setTransactionsData(res?.transactions);
-    }
+  const addTransactionHandler = (tradingParty, counterParty, amount) => {
+    addAction(tradingParty, counterParty, amount, setTransactionsData);
   };
+
+  const compressHandler = () => {
+    compressAction(setTransactionsData);
+  }
 
   const openAddDialogHandler = () => {
     setIsAddDialogOpen(true);
-  }
+  };
 
   const closeAddDialogHandler = () => {
     setIsAddDialogOpen(false);
-  }
+  };
 
   const payingData = transactionsData
     ?.map?.(payTrans => ({
       name: payTrans.tradingParty,
       amount: Math.abs(payTrans.amount),
+      id: payTrans.id,
     }));
 
   const receivingData = transactionsData
-    ?.map?.(payTrans => ({
-    name: payTrans.counterparty,
-    amount: payTrans.amount,
+    ?.map?.(receiveTrans => ({
+    name: receiveTrans.counterParty,
+    amount: receiveTrans.amount,
+    id: receiveTrans.id,
   }));
 
   return (
@@ -61,14 +53,18 @@ const TransactionsPage = () => {
           Paying
         </styles.Title>
       <TransactionsTable data={ payingData }/>
-      <Button onClickHandler={ openAddDialogHandler } label='Add New Transaction'/>
+      <Button 
+        onClickHandler={ openAddDialogHandler } 
+        label='Add New Transaction'/>
     </styles.TableWrapper>
     <styles.TableWrapper>
         <styles.Title>
           Receiving
         </styles.Title>
       <TransactionsTable data={ receivingData }/>
-      <Button onClickHandler={() => {}} label='Compress Transactions'/>
+      <Button 
+        onClickHandler={ compressHandler } 
+        label='Compress Transactions'/>
     </styles.TableWrapper>
       </styles.TablesWrapper>
       <AddTransactionDialog 
--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,1 +1,2 @@
 export const TRANSACTIONS_URL = "http://localhost:3001/transactions";
+export const COMPRESS_URL = "http://localhost:3001/transactions/compress";
